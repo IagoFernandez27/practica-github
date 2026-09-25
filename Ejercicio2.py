41828557daf6e03bf9b3c7e2d625ce84e4fe3c37
IagoFernandez27 <iago.fernandez@campus.xaloc.org>
2026-09-25 09:50:18 +0200

    Refactor Ejercicio2.py

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,5 +1,6 @@
 #Programa que introduzca por teclado tres tipos de variables y se muestren por pantalla en el siguiente orden: número entero, texto y número decimal.
 #El programa muestra por pantalla un número, una letra y un número decimal introducido previamente#
+#modificación#
 variable1=(input("introduce un número:"))
 variable2=(input("introduce una letra:"))
 variable3=float(input("introduce un número decimal:"))
